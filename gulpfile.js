e717be0540bf188672e8672fc238f353881cefb0
shenyu <samfec@163.com>
2016-08-06 21:14:13 +0800

    edit gulpfile less => css ,html => minHtml, img =>minImg

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
-//导入工具包 require('node_modules里对应模块')
-var gulp = require('gulp'), //本地安装gulp所用到的地方
-    less = require('gulp-less');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    path = require('path'),
+    htmlmin = require('gulp-htmlmin'),
+    cssBase64 = require('gulp-css-base64'),
+    imagemin = require('gulp-imagemin');
 
-//定义一个testLess任务（自定义任务名称）
-gulp.task('testLess', function () {
-    gulp.src('src/less/index.less') //该任务针对的文件
-        .pipe(less()) //该任务调用的模块
-        .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
+var originalPath = path.join(__dirname, 'public');
+var projectPath = path.join(__dirname, 'slotMachine');
+
+gulp.task('compileLess', function () {
+    gulp.src(path.join(originalPath, 'less/**/*.less'))
+        .pipe(less())
+        .pipe(cssBase64())
+        .pipe(gulp.dest(path.join(projectPath, 'public/css')));
 });
 
-gulp.task('default', ['testLess']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+gulp.task('moveHtml', function () {
+    gulp.src(path.join(__dirname, "index.html"))
+        // .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(projectPath))
+});
 
-//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
-//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
-//gulp.dest(path[, options]) 处理完后文件生成路径
+gulp.task('moveImages', function () {
+    gulp.src(path.join(originalPath, 'images/**/*.*'))
+        .pipe(imagemin())
+        .pipe(gulp.dest(path.join(projectPath, 'public/images')))
+})
+
+gulp.task('default', [
+    'compileLess',
+    'moveHtml',
+    'moveImages'
+]);
+
+
+
