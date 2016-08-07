f174c20066590ef467e828ed243f6e779b16cde8
shenyu <samfec@163.com>
2016-08-07 22:07:38 +0800

    .

---
+++ public/js/test.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+
+
+let funcTest = (r) => {
+    return 2 * PI * r;
+}
---
+++ slotMachine/public/js/test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var PI = 3.14;
+
+var funcTest = function funcTest(r) {
+    return 2 * PI * r;
+};
