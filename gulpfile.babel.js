07c0ad2fbd87c7f5b1c5da97726bdcc85ca83551
shenyu <samfec@163.com>
2016-08-07 21:51:02 +0800

    add all file in atom

---
+++ gulpfile.babel.js
@@ -0,0 +1,49 @@
+'use strict';
+import path from 'path';
+import gulp from 'gulp';
+import less from 'gulp-less';
+import htmlmin from 'gulp-htmlmin';
+import pug from 'gulp-pug';
+import cssBase64 from 'gulp-css-base64';
+import imagemin from 'gulp-imagemin';
+
+//postcss相关插件
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import cssnext from 'cssnext';
+import precss from 'precss';
+
+const PATH = {
+    originalPath: path.join(__dirname, 'public'),
+    projectPath: path.join(__dirname, 'slotMachine')
+}
+
+gulp.task('compileLess', () => {
+    const processors = [
+        autoprefixer,
+        cssnext,
+        precss
+    ];
+    gulp.src(`${PATH.originalPath}/less/**/*.less`)
+        .pipe(less())
+        .pipe(cssBase64())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(`${PATH.projectPath}/public/css`))
+})；
+
+gulp.task('moveHtml', () => {
+    gulp.src(`${__dirname}/index.html`)
+        .pipe(gulp.dest(PATH.projectPath));
+});
+
+gulp.task('moveImages', () => {
+    gulp.src(`${PATH.originalPath}/images/**/*.*`)
+        .pipe(imagemin())
+        .pipe(gulp.dest(`${PATH.projectPath}/public/images`));
+});
+
+gulp.task('default', [
+    'compileLess',
+    'moveHtml',
+    'moveImages'
+]);
